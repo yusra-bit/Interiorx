8dd59e0f97b288247641fe50607eb428ed909bfa
Yusra Abdi <yusra4411@gmail.com>
2023-12-04 15:38:38 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+      'hero-pattern': "url('./img/background.png' opacity:2rem",
+      }
+
+    },
   },
   plugins: [],
 }
